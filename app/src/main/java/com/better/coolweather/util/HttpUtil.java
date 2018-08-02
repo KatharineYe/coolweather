1dacf5de81b6d5a6eefabb5321ed9170d9f2b786
Katharine Ye <yeting_1984@yeah.net>
2018-08-02 15:34:17 +0800

    no message

---
+++ app/src/main/java/com/better/coolweather/util/HttpUtil.java
@@ -0,0 +1,17 @@
+package com.better.coolweather.util;
+
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by kaye on 7/27/18.
+ */
+
+public class HttpUtil {
+    public static void sendOKHttpRequest(String address, okhttp3.Callback callback) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
